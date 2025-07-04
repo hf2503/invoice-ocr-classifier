032acf6f6620c699e7119de2c5f14142f1792adb
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-07-04 18:07:52 +0200

    modif dag

---
+++ dags/invoice_dag.py
@@ -0,0 +1,18 @@
+# from airflow import dag
+# from airflow.operators.python import PythonOperator
+# from airflow.operators.bash import BashOperator
+# from datetime import datetime
+# import sys
+# import os
+
+# #script's path
+# sys.path.append('/opt/airflow/src')
+
+# default_args = {
+#     'start_date': datetime(2024,1,1),
+#     'retries':1
+# }
+
+
+# with DAG("my_first_dag",
+#          sta)
