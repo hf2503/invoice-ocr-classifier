dff0105d8c7bf1dbd4b5d89bc68b093bae189652
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-07-04 19:07:16 +0200

    creation du dag

--- dags/invoice_dag.py
+++ dags/invoice_dag.py
@@ -1,18 +1,22 @@
-# from airflow import dag
-# from airflow.operators.python import PythonOperator
-# from airflow.operators.bash import BashOperator
-# from datetime import datetime
-# import sys
-# import os
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 
-# #script's path
-# sys.path.append('/opt/airflow/src')
-
-# default_args = {
-#     'start_date': datetime(2024,1,1),
-#     'retries':1
-# }
+from main import convert_invoice_pdf
 
 
-# with DAG("my_first_dag",
-#          sta)
+
+with DAG(
+    dag_id = "lux_invoice",
+    start_date=datetime(2025,1,1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+    task = PythonOperator(
+        task_id = "convert_invoice_pdf",
+        python_callable= convert_invoice_pdf
+    )
