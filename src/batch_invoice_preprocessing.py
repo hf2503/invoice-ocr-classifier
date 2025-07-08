caa7f2bde7a8b1e5bb6894a1ace8bf0b8db4aadf
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-07-08 18:34:14 +0200

    ajout de logger

--- src/batch_invoice_preprocessing.py
+++ src/batch_invoice_preprocessing.py
@@ -1,9 +1,20 @@
 import os
 import traceback
 import config
+import logging
 
 from process_invoice_pdf import process_invoice_pdf
 from reclassify_unidentified_invoices import reclassify_unidentified_invoices
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler('logs/invoice_processing.log',mode='a',encoding='utf_8'),
+        logging.StreamHandler()
+    ]
+)
+
 
 def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_PDF_FOLDER):
     list_pdf_input_folder = os.listdir(input_pdf_folder)
@@ -11,6 +22,7 @@ def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_PDF_FOLDER):
     for filename in list_pdf_input_folder:
         if filename.endswith('.pdf'):
             input_pdf_path = os.path.join(input_pdf_folder,filename)
+            logging.info("input_pdf_folder:%s:",input_pdf_folder)
             
             try:
                 process_invoice_pdf(input_pdf=input_pdf_path)
