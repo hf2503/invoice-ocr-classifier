0daed0386da2cb6ed84f741e9eb3ee72b13543b2
hf2503 <hfmole@hotmail.com>
2025-09-19 17:46:33 +0200

    ajout d'une ligne pour supprimer le dataset_final

--- src/delete.py
+++ src/delete.py
@@ -2,3 +2,4 @@ import subprocess
 
 subprocess.run("rm -rf ../data/output/*",shell=True)
 subprocess.run("rm -rf ../data/train/*",shell=True)
+subprocess.run("rm -rf ../train_final/dataset/*",shell=True)
