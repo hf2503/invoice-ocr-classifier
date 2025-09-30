99b120e277cf6a67249c4795715af92f47e35280
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-09-30 18:13:52 +0200

    modif de nom output ---> facture

--- src/delete.py
+++ src/delete.py
@@ -1,5 +1,5 @@
 import subprocess
 
-subprocess.run("rm -rf ../data/output/*",shell=True)
+subprocess.run("rm -rf ../data/factures/*",shell=True)
 subprocess.run("rm -rf ../data/train/*",shell=True)
 subprocess.run("rm -rf ../train_final/dataset/*",shell=True)
