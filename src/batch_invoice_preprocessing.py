1305dafeaa3ff68d147ee405d4ff4fb3dae9fd46
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-09-30 18:12:51 +0200

    archive_facture brut en cours de réalisation

--- src/batch_invoice_preprocessing.py
+++ src/batch_invoice_preprocessing.py
@@ -1,9 +1,12 @@
 import os
 import traceback
-# import config
+import shutil
 import logging
+import datetime
+import csv
 
 from . import config
+from .utils import extract_SHA1
 from .process_invoice_pdf import process_invoice_pdf
 # from reclassify_unidentified_invoices import reclassify_unidentified_invoices
 
@@ -17,7 +20,43 @@ logging.basicConfig(
 )
 
 
-def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_PDF_FOLDER):
+def archive_csv(file_path,csv_path=config.ARCHIVE_CSV):
+
+    if not os.path.exists(csv_path):
+        os.path.join(config.ARCHIVE_DIR,csv)
+
+    filename = os.path.basename(file_path)
+    now = datetime.now()
+    date = now.strftime("%d-%m-%Y")
+    heure = now.strftime("%M-%M")
+    pdf_hash = extract_SHA1(file_path)
+
+    with open(file_path,'w',newline='') as f :
+        writer = csv.writer(f,delimiter=';')
+        writer.writerow([filename,date,heure,pdf_hash])
+
+
+
+
+
+
+
+
+
+
+
+
+
+def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_DIR,
+                                archive_input_pdf_folder = config.ARCHIVE_DIR):
+
+
+    #vérification de l'existence du dossier data/facture brutes
+    os.makedirs(input_pdf_folder,exist_ok=True)
+
+    #vérification de l'exitence du dossier facture brutes archives
+    os.makedirs(archive_input_pdf_folder,exist_ok=True)
+
     list_pdf_input_folder = os.listdir(input_pdf_folder)
         
     if len(list_pdf_input_folder)==0:
@@ -31,6 +70,11 @@ def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_PDF_FOLDER):
             
             try:
                 process_invoice_pdf(input_pdf=input_pdf_path)
+
+                #archivage du fichier brut 
+                shutil.move(input_pdf_path,archive_input_pdf_folder)
+
+
             except Exception as e:
                 print(f"on eu l'erreur suivante avec le fichier {filename} : {e}")
                 traceback.print_exc()
