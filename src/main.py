7aa2988f04a8053be29fd61fef0fe6267a0ef5fb
hf2503 <hfmole@hotmail.com>
2025-10-02 17:34:41 +0200

    suppression de reclassify unidentified invoice

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from .batch_invoice_preprocessing import batch_invoice_preprocessing
 from .process_invoice_pdf import process_invoice_pdf
-from .reclassify_unidentified_invoices import reclassify_unidentified_invoices
+# from .reclassify_unidentified_invoices import reclassify_unidentified_invoices
 
 if __name__ == "__main__":
     batch_invoice_preprocessing()
