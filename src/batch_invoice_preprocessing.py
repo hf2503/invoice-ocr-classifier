7f57537f5d9e86f8843653759a922947103c4d9b
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-10-24 18:09:23 +0200

    ajout de la variable already_archived

--- src/batch_invoice_preprocessing.py
+++ src/batch_invoice_preprocessing.py
@@ -74,6 +74,8 @@ def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_DIR,
     os.makedirs(raw_invoice_folder,exist_ok=True)
 
     list_pdf_input_folder = os.listdir(input_pdf_folder)
+
+    already_archived = set(os.listdir(raw_invoice_folder))
         
     if len(list_pdf_input_folder)==0:
         print("there are not invoices into the directory input")
@@ -81,38 +83,43 @@ def batch_invoice_preprocessing(input_pdf_folder = config.INPUT_DIR,
     
     
     for filename in list_pdf_input_folder:
-        if filename.endswith('.pdf'):
-            
-            # input_pdf_path = os.path.join(input_pdf_folder,filename)
-            # logging.info("input_pdf_folder:%s:",input_pdf_folder)
+
+        if not filename.endswith('.pdf'):
+            logging.info(f"le fichier {filename} doit être au format pdf")
+            continue
+
+
+        if filename in already_archived:
+            print("le fichier a déjà été traité")
+            continue
+
             
             #enregistrement dans le fichier archive_facture.csv
 
-            file_path = os.path.join(input_pdf_folder,filename)
-            logging.info("input_pdf_folder:%s:",file_path)
-            archive_csv(file_path)
+        file_path = os.path.join(input_pdf_folder,filename)
+        logging.info("input_pdf_folder:%s:",file_path)
+        archive_csv(file_path)
             
-            try:
-                process_invoice_pdf(input_pdf=file_path)
+        try:
+            process_invoice_pdf(input_pdf=file_path)
 
-                #sauvegarde du fichier pdf brut dans un le dossier  facture brute archive
-                archive_path = os.path.join(archive_input_pdf_folder,raw_invoice_folder,filename)
+            #sauvegarde du fichier pdf brut dans un le dossier  facture brute archive
+            archive_path = os.path.join(archive_input_pdf_folder,raw_invoice_folder,filename)
 
 
-                logging.info(f"le fichier brute pdf {filename} a été traité avec succés")
+            logging.info(f"le fichier brute pdf {filename} a été traité avec succés")
                 
-                if not os.path.exists(archive_path):
-                    shutil.move(file_path,archive_path)
+            if not os.path.exists(archive_path):
+                shutil.move(file_path,archive_path)
 
-                else:
-                    logging.warning("l'archive existe déjà")
+            else:
+                logging.warning("l'archive existe déjà")
                     #pas de deplacement et supression du fichier brute traité
 
-                    logging.debug(f"le file à supprimer est : {file_path}")
-                    os.remove(file_path)
+                logging.debug(f"le fichier à supprimer est : {file_path}")
+                os.remove(file_path)
 
-            except Exception as e:
-                print(f"on eu l'erreur suivante avec le fichier {filename} : {e}")
-                traceback.print_exc()
-        else:
-            raise TypeError(f"le fichier {filename} doit être au format pdf")
+        except Exception as e:
+            print(f"on eu l'erreur suivante avec le fichier {filename} : {e}")
+            traceback.print_exc()
+
