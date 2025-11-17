f2606266ba45265df62e1bf32ed2f483316a5a07
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-11-17 17:58:23 +0100

    translation

--- src/config.py
+++ src/config.py
@@ -5,24 +5,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-#project_path
+# Directories
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR,'data')
-INPUT_DIR = os.path.join(DATA_DIR,'factures_brutes_a_traiter')
-OUTPUT_DIR = os.path.join(DATA_DIR,'factures_classees')
-ARCHIVE_DIR = os.path.join(DATA_DIR,'factures_brutes_archivees')
-SUIVI_DIR = os.path.join(DATA_DIR,'suivi')
+INPUT_DIR = os.path.join(DATA_DIR,'raw_invoices_to_process')
+OUTPUT_DIR = os.path.join(DATA_DIR,'classified_invoices')
+ARCHIVE_DIR = os.path.join(DATA_DIR,'archived_raw_invoices')
+SUIVI_DIR = os.path.join(DATA_DIR,'tracking')
 
 #dossier facture classé (format png) et dossier facture brutes (format pdf)
-FACTURE_CLASSEES_DIR = os.path.join(SUIVI_DIR,'factures_traitees_archive')
-FACTURES_BRUTES_DIR = os.path.join(ARCHIVE_DIR,'factures_brutes')
+FACTURE_CLASSEES_DIR = os.path.join(SUIVI_DIR,'processed_invoices_archive')
+FACTURES_BRUTES_DIR = os.path.join(ARCHIVE_DIR,'raw_invoices')
 
 #csv path
-SUPPLIER_LIST_CSV = os.path.join(DATA_DIR,"list_supplier.csv")
-COMPANY_LIST_CSV = os.path.join(DATA_DIR,"list_company.csv")
-ARCHIVE_CSV = os.path.join(ARCHIVE_DIR,'factures__brutes_archivees.csv')
-SUIVI_CSV = os.path.join(SUIVI_DIR,'suivi_factures_traitees.csv')
-RESULTAT_CSV = os.path.join(OUTPUT_DIR,'resultat.csv')
+SUPPLIER_LIST_CSV = os.path.join(DATA_DIR,"supplier_list.csv")
+COMPANY_LIST_CSV = os.path.join(DATA_DIR,"company_list.csv")
+ARCHIVE_CSV = os.path.join(ARCHIVE_DIR,'archived_raw_invocies.csv')
+SUIVI_CSV = os.path.join(SUIVI_DIR,'processed_invoices_tracking.csv')
+RESULTAT_CSV = os.path.join(OUTPUT_DIR,'results.csv')
 
 #read csv
 supplier_df = pd.read_csv(SUPPLIER_LIST_CSV ,sep=',')
