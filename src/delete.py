2baf005f7e3ac6e0ce6b3b326b98832cefbc88e7
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-11-21 18:42:17 +0100

    maj delete

--- src/delete.py
+++ src/delete.py
@@ -1,5 +1,14 @@
 import subprocess
+import shutil
 
-subprocess.run("rm -rf ../data/classified_invoices/*",shell=True)
-subprocess.run("rm -rf ../data/tracking/*",shell=True)
-subprocess.run("rm -rf ../data/archived_raw_invoices/*",shell=True)
+# subprocess.run("rm -rf ../data/classified_invoices/*",shell=True)
+# subprocess.run("rm -rf ../data/tracking/*",shell=True)
+# subprocess.run("rm -rf ../data/archived_raw_invoices/*",shell=True)
+
+# data_path = r'C:\Users\User\Documents\LUX_INVOICE_2\data'
+
+shutil.rmtree(r'C:\Users\User\Documents\LUX_INVOICE_2\data\archived_raw_invoices')
+shutil.rmtree(r'C:\Users\User\Documents\LUX_INVOICE_2\data\classified_invoices')
+shutil.rmtree(r'C:\Users\User\Documents\LUX_INVOICE_2\data\tracking')
+
+shutil.copy(r'C:\Users\User\Documents\LUX_INVOICE_2\invoice_issue\SPAM\SPAM FRS 03 11 25.pdf',r'C:\Users\User\Documents\LUX_INVOICE_2\data\raw_invoices_to_process')
