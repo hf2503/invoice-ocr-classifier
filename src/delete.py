e4cf9274533a7afe10405b9cffc6702098cc552d
hf2503 <hfmole@hotmail.com>
2025-11-26 14:42:49 +0100

    maj brouillon+ delete

--- src/delete.py
+++ src/delete.py
@@ -11,4 +11,7 @@ shutil.rmtree(r'C:\Users\Windows\Documents\LUX_INVOICE_2\data\archived_raw_invoi
 shutil.rmtree(r'C:\Users\Windows\Documents\LUX_INVOICE_2\data\classified_invoices',ignore_errors=True)
 shutil.rmtree(r'C:\Users\Windows\Documents\LUX_INVOICE_2\data\tracking',ignore_errors=True)
 
-shutil.copy(r'C:\Users\Windows\Documents\LUX_INVOICE_2\invoice_issue\SPAM\SPAM FRS 16 09 2025.pdf',r'C:\Users\Windows\Documents\LUX_INVOICE_2\data\raw_invoices_to_process')
+shutil.copy(r'C:\Users\Windows\Documents\LUX_INVOICE_2\invoice_issue\SPESA FRS 03 11 25 copy.pdf',r'C:\Users\Windows\Documents\LUX_INVOICE_2\data\raw_invoices_to_process')
+
+shutil.copy(r'C:\Users\Windows\Documents\LUX_INVOICE_2\invoice_issue\spt frs 13 06 25.pdf',r'C:\Users\Windows\Documents\LUX_INVOICE_2\data\raw_invoices_to_process')
+
