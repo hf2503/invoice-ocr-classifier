90b0bd77be0dc27e37b6c1e3a4f934945e11ba7f
hf2503 <97042427+hf2503@users.noreply.github.com>
2025-12-18 18:51:28 +0100

    maj

--- src/utils.py
+++ src/utils.py
@@ -564,11 +564,14 @@ def image_to_text(path:str,
 
 def resolve_company(company_csv:pd.DataFrame,
                     company_name,
-                    output_dir:str) :
+                    output_dir:str,
+                    list_company_invoice:list,
+                    new_company:str) :
         
     if isinstance(company_name, tuple):
         parent_company = company_name[0]
         company_name = company_name[1]
+
         #directory_parent_match = company_csv.loc[company_csv['parent_company'] == company_name[0],'parent_company'].values
         directory_parent_match = company_csv.loc[company_csv['company_name_invoice'] == parent_company,'parent_company'].values
         directory_company_match = company_csv.loc[company_csv['company_name_invoice'] == company_name,'company_name_registery'].values
@@ -577,39 +580,9 @@ def resolve_company(company_csv:pd.DataFrame,
         logging.info("directory_parent_match_debogage_1 LBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB: %s",directory_parent_match)
         logging.info("directory_company_match_debogage_1 LCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC: %s",directory_company_match)
 
-        if directory_company_match.size == 0 : 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         if directory_company_match.size > 1 :
-                    
+                
             directory_parent_company = directory_parent_match[0]
-
-            # #------------feature parent_company-----------
-
-            # row['parent_company'] = directory_parent_match[0]
-
-            # #------feature company_name-------
-            # row['company_name'] = directory_parent_match[1]
 
             logging.info("directory_parent_company:%s:",directory_parent_company)
 
@@ -619,35 +592,46 @@ def resolve_company(company_csv:pd.DataFrame,
 
             directory_company_path = make_directory_mother_company(output_dir,directory_parent_company,directory_company)
 
-            # logging.info("directory_company_path:%s:",directory_company_path)
-
-            # logging.info("using directory for registered company '%s' : %s ",directory_company,directory_company_path)
+            return directory_company_path
 
         else:
 
-            #------------feature parent_company-----------
-
-            # row['parent_company'] = directory_parent_match[0]
-            
-            # logging.info("parent_company LDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD: %s",row['parent_company']) 
-
-            #------feature company_name-------
-
-            # row['company_name'] = directory_company_match[0]
-            
-            # logging.info("'company_name' LEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE: %s",row['company_name']) 
-            
             directory_parent_company = directory_parent_match[0]
+            logging.info("direcory_parent_company LDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD: %s",directory_parent_company)
+
             directory_company = directory_company_match[0]
-            
-            #-------------debogage----------------
-            
-            # logging.info("directory_parent_company_debogage_4: %s",directory_parent_company)
-            # logging.info(" directory_company_debogage_4: %s",directory_company)
-            
-            #-------------fin_debogage------------------
-            
+            logging.info("company_name LDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD: %s",directory_company)
+
             directory_company_path = make_directory_mother_company(output_dir,directory_parent_company,directory_company)
+
+            return directory_company_path
+
+
+    elif company_name in list_company_invoice or company_name == 'new_company':
+        directory_company_match = company_csv.loc[company_csv['company_name_invoice'] == company_name,'company_name_registery'].values
+
+
+        if directory_company_match.size > 0:
+            logging.info(f"directory_company_match : {directory_company_match}")   
+        
+            directory_company = directory_company_match[0]   
+            directory_company_path = make_directory_company(output_dir,directory_company)
+            logging.info("using directory for registered company '%s' : %s ",directory_company,directory_company_path)
+            logging.info(directory_company_path)
+        
+        else :
+
+            logging.info("company name '%s' not found in registry; fallback to '%s'",company_name,new_company)
+            directory_company_path = make_directory_company(output_dir,new_company)
+    
+    else:
+        logging.warning("company name '%s' not matched anywhere; fallback to '%s'", company_name, new_company)
+        directory_company_path = make_directory_company(output_dir, new_company)
+
+
+
+
+
 
 
 
